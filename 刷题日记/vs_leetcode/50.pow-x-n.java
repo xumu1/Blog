79b5a3a0ec71b6815eaa0620bd046b0fa37ce270
xumu <932851779@qq.com>
2020-08-28 15:02:04 +0800

    m

---
+++ 刷题日记/vs_leetcode/50.pow-x-n.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=50 lang=java
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+class Solution {
+    public double myPow(double x, int n) {
+
+    }
+}
+// @lc code=end
+
