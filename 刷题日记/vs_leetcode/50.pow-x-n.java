f4838e200123dfa3a0bb540dde529c2d5ebc27fd
xumu <932851779@qq.com>
2020-08-31 22:24:17 +0800

    leetcode

--- 刷题日记/vs_leetcode/50.pow-x-n.java
+++ 刷题日记/vs_leetcode/50.pow-x-n.java
@@ -7,8 +7,33 @@
 // @lc code=start
 class Solution {
     public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (x == 1) {
+            return 1;
+        }
+        long N = n;
+        if (n > 0) {
+            return dp(x, N);
+        } else {
+            return 1 / dp(x, -N);
+        }
+    }
 
+    public double dp(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n % 2 == 0) {
+            // 偶数
+            double item = dp(x, n / 2);
+            return item * item;
+        } else {
+            // 奇数
+            double item = dp(x, (n - 1) / 2);
+            return item * item * x;
+        }
     }
 }
 // @lc code=end
-
