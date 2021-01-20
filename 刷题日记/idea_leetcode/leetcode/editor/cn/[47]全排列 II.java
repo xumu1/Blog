beb23a18030b116faf586a105de275f2bc6628c1
haolong.sun <haolong.sun@ly.com>
2021-01-20 20:33:55 +0800

    lc

---
+++ 刷题日记/idea_leetcode/leetcode/editor/cn/[47]全排列 II.java
@@ -0,0 +1,42 @@
+//给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,1,2]
+//输出：
+//[[1,1,2],
+// [1,2,1],
+// [2,1,1]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1,2,3]
+//输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 8 
+// -10 <= nums[i] <= 10 
+// 
+// Related Topics 回溯算法 
+// 👍 563 👎 0
+
+
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
