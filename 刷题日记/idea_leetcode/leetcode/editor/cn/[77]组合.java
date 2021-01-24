7f538439aae9350d0b97ee5e2e3886d4723a9f56
haolong.sun <haolong.sun@ly.com>
2021-01-24 21:26:22 +0800

    lc

---
+++ 刷题日记/idea_leetcode/leetcode/editor/cn/[77]组合.java
@@ -0,0 +1,27 @@
+//给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。 
+//
+// 示例: 
+//
+// 输入: n = 4, k = 2
+//输出:
+//[
+//  [2,4],
+//  [3,4],
+//  [2,3],
+//  [1,2],
+//  [1,3],
+//  [1,4],
+//] 
+// Related Topics 回溯算法 
+// 👍 472 👎 0
+
+
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
