956b9b014998d90b2c787a8b7005d16ec4441fdc
haolong.sun <haolong.sun@ly.com>
2021-01-26 17:28:41 +0800

    lc

---
+++ 刷题日记/idea_leetcode/leetcode/editor/cn/[82]删除排序链表中的重复元素 II.java
@@ -0,0 +1,33 @@
+//给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。 
+//
+// 示例 1: 
+//
+// 输入: 1->2->3->3->4->4->5
+//输出: 1->2->5
+// 
+//
+// 示例 2: 
+//
+// 输入: 1->1->1->2->3
+//输出: 2->3 
+// Related Topics 链表 
+// 👍 437 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.*/
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
