c4b797b78e2d15cea9cb0feb7de2638b9fde10c4
haolong.sun <haolong.sun@ly.com>
2021-02-03 10:31:30 +0800

    lc

--- 刷题日记/idea_leetcode/leetcode/editor/cn/[91]解码方法.java
+++ 刷题日记/idea_leetcode/leetcode/editor/cn/[91]解码方法.java
@@ -65,7 +65,32 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int numDecodings(String s) {
-
+        int len = s.length();
+        int[] dp = new int[len + 1];
+        //0个数 一种
+        dp[0] = 1;
+        //1个数 一种
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        //dp数组中，下标都+1，表示字符个数为下标时的方法总数
+        for (int i = 1; i < len; i++) {
+            //10-26
+            if ((s.charAt(i - 1) == '1') || (s.charAt(i - 1) == '2' && s.charAt(i) <= '6')) {
+                //10或20
+                if (s.charAt(i) == '0') {
+                    dp[i + 1] = dp[i - 2 + 1];
+                } else {
+                    dp[i + 1] = dp[i - 1 + 1] + dp[i - 2 + 1];
+                }
+            } else {
+                //比如40，50
+                if (s.charAt(i) == '0') {
+                    return 0;
+                } else {
+                    dp[i + 1] = dp[i - 1 + 1];
+                }
+            }
+        }
+        return dp[len];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
