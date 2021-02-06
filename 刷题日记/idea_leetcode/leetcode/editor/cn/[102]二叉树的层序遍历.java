8d0bf177f683b63d7df8ef06c915855c48a5467b
haolong.sun <haolong.sun@ly.com>
2021-02-07 00:04:31 +0800

    lc

---
+++ 刷题日记/idea_leetcode/leetcode/editor/cn/[102]二叉树的层序遍历.java
@@ -0,0 +1,47 @@
+//给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。 
+//
+// 
+//
+// 示例： 
+//二叉树：[3,9,20,null,null,15,7], 
+//
+// 
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回其层序遍历结果： 
+//
+// 
+//[
+//  [3],
+//  [9,20],
+//  [15,7]
+//]
+// 
+// Related Topics 树 广度优先搜索 
+// 👍 765 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
