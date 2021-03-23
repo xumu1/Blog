90acc34c563e347c4c151d286e9512f59ed40613
haolong.sun <haolong.sun@ly.com>
2021-03-23 18:55:37 +0800

    lc

--- 刷题日记/idea_leetcode/leetcode/editor/cn/[25]K 个一组翻转链表.java
+++ 刷题日记/idea_leetcode/leetcode/editor/cn/[25]K 个一组翻转链表.java
@@ -61,22 +61,22 @@
 /**
  * Definition for singly-linked list.
  */
-//public class ListNode {
-//    int val;
-//    ListNode next;
-//
-//    ListNode() {
-//    }
-//
-//    ListNode(int val) {
-//        this.val = val;
-//    }
-//
-//    ListNode(int val, ListNode next) {
-//        this.val = val;
-//        this.next = next;
-//    }
-//}
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
