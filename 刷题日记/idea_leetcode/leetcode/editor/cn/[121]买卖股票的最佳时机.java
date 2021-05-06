565e0d2b24bedc44343257f7817b39c700483e7c
haolong.sun <haolong.sun@ly.com>
2021-05-07 01:20:20 +0800

    Default Changelist

--- 刷题日记/idea_leetcode/leetcode/editor/cn/[121]买卖股票的最佳时机.java
+++ 刷题日记/idea_leetcode/leetcode/editor/cn/[121]买卖股票的最佳时机.java
@@ -38,6 +38,13 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int res = 0;
+        for (int i = 1; i < prices.length; i++) {
+            min = Math.min(prices[i],min);
+            res = Math.max(res,prices[i]-min);
+        }
+        return res;
 
     }
 }
---
+++ 刷题日记/idea_leetcode/leetcode/editor/cn/[415]字符串相加.java
@@ -0,0 +1,23 @@
+//给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// num1 和num2 的长度都小于 5100 
+// num1 和num2 都只包含数字 0-9 
+// num1 和num2 都不包含任何前导零 
+// 你不能使用任何內建 BigInteger 库， 也不能直接将输入的字符串转换为整数形式 
+// 
+// Related Topics 字符串 
+// 👍 364 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String addStrings(String num1, String num2) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
