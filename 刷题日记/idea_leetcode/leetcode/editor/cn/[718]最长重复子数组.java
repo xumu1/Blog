8e76cba3b18ec6b21757635e92cd9380bb80cd22
haolong.sun <sunhaolong@fenbi.com>
2021-05-26 00:47:29 +0800

    m

--- 刷题日记/idea_leetcode/leetcode/editor/cn/[718]最长重复子数组.java
+++ 刷题日记/idea_leetcode/leetcode/editor/cn/[718]最长重复子数组.java
@@ -26,7 +26,25 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    /**
+     * 整一个dp数组，dp[i][j]代表nums1到i为止和nums2到j为止最大长度
+     */
     public int findLength(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        int max = 0;
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (nums1[i-1] == nums2[j-1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    max = Math.max(max,dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return max;
 
     }
 }
