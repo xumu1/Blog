515a3b94ff2760a478cb44644482f3b5bce8a3b5
haolong.sun <sunhaolong@fenbi.com>
2021-06-02 01:10:10 +0800

    MOD

--- 刷题日记/idea_leetcode/leetcode/editor/cn/[剑指 Offer 36]二叉搜索树与双向链表.java
+++ 刷题日记/idea_leetcode/leetcode/editor/cn/[剑指 Offer 36]二叉搜索树与双向链表.java
@@ -55,8 +55,34 @@ class Node {
 };
 */
 class Solution {
+
+    /**
+     * 沿中序遍历遍历一边即可
+     */
+    Node head, pre;
     public Node treeToDoublyList(Node root) {
-        
+        if (root == null) {
+            return null;
+        }
+        dfs(root);
+        pre.right = head;
+        head.left = pre;
+        return head;
+    }
+
+    public void dfs(Node cur) {
+        if (cur == null) {
+            return;
+        }
+        dfs(cur.left);
+        if (pre == null) {
+            head = cur;
+        } else {
+            pre.right = cur;
+        }
+        cur.left = pre;
+        pre = cur;
+        dfs(cur.right);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
