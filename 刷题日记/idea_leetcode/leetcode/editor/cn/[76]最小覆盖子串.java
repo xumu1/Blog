17da8d56232248d4d7aacd229dd4b50a91a9b799
haolong.sun <sunhaolong@fenbi.com>
2021-06-02 18:17:25 +0800

    MOD

---
+++ 刷题日记/idea_leetcode/leetcode/editor/cn/[76]最小覆盖子串.java
@@ -0,0 +1,72 @@
+//给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。 
+//
+// 注意：如果 s 中存在这样的子串，我们保证它是唯一的答案。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "ADOBECODEBANC", t = "ABC"
+//输出："BANC"
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "a", t = "a"
+//输出："a"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length, t.length <= 105 
+// s 和 t 由英文字母组成 
+// 
+//
+// 
+//进阶：你能设计一个在 o(n) 时间内解决此问题的算法吗？ Related Topics 哈希表 双指针 字符串 Sliding Window 
+// 👍 1179 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s == null || s.length() == 0 || t == null || t.length() == 0){
+            return "";
+        }
+        int[] need = new int[128];
+        //记录需要的字符的个数
+        for (int i = 0; i < t.length(); i++) {
+            need[t.charAt(i)]++;
+        }
+        //l是当前左边界，r是当前右边界，size记录窗口大小，count是需求的字符个数，start是最小覆盖串开始的index
+        int l = 0, r = 0, size = Integer.MAX_VALUE, count = t.length(), start = 0;
+        while (r < s.length()) {
+            char c = s.charAt(r);
+            if (need[c] > 0) {
+                count--;
+            }
+            need[c]--;
+            if (count == 0) {
+                while (l < r && need[s.charAt(l)] < 0) {
+                    need[s.charAt(l)]++;
+                    l++;
+                }
+                if (r - l + 1 < size) {
+                    size = r - l + 1;
+                    start = l;
+                }
+                need[s.charAt(l)]++;
+                l++;
+                count++;
+            }
+            r++;
+        }
+        return size == Integer.MAX_VALUE ? "" : s.substring(start, start + size);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
