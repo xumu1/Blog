4b3533a37cad54a8d08d530414e2dcaf26713e2e
xumu <sunhaolong.shl@alibaba-inc.com>
2021-07-23 10:03:08 +0800

    m

--- demo/testSort.java
+++ demo/testSort.java
@@ -10,22 +10,35 @@ import java.util.Locale;
  */
 public class testSort {
     public static void main(String[] args) {
-//        String s = "c.ba";
-//        char[] chars = s.toCharArray();
-//        Arrays.sort(chars);
-//        System.out.println(chars);
-//        String[] split = s.split(".");
-//        System.out.println(split);
-//        int[] a = new int[]{5,2,3};
-//        System.out.println(1);
-        String mac = "01-00-5e-xx-xx-xx";
-        String s = mac.toUpperCase(Locale.ROOT);
-        System.out.println(s);
-        StringBuffer str5 = new StringBuffer();
-        for (String item : s.split("-")) {
-            str5.append(item);
+        int[] test = new int[]{1,2,2,1,4,1,2,55,547,5};
+        quickSort(test,0,test.length-1);
+        System.out.println(1);
+    }
+
+    private static void quickSort(int[] nums,int l,int r){
+        if (l<r){
+            int mid = partition(nums,l,r);
+            quickSort(nums,l,mid-1);
+            quickSort(nums,mid+1,r);
         }
-        System.out.println(str5);
+    }
+    private static int partition(int[] nums,int l,int r ){
+        int target = nums[r];
+        int little = l-1;
+        for (int i = l; i < r; i++) {
+            if (nums[i] < target){
+                little++;
+                swap(nums,little,i);
+            }
+        }
+        little++;
+        swap(nums,little,r);
+        return little;
+    }
+    private static void swap(int[] nums, int l,int r){
+        int tmp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = tmp;
     }
 
 }
